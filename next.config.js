bada6eec9a0acee539d74a6050dab74675e57c94
pasang1554 <stanzinpasang03@gmail.com>
2025-03-26 21:15:10 +0530

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/adventure-website',
+  assetPrefix: '/adventure-website/',
 }
 
 module.exports = nextConfig 
